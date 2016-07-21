62f35462241e0f9d78eead9f4dce992c6f962121
Joe Carlino <joecarlino84@gmail.com>
2016-07-20 22:23:27 -0400

    second commit 5:00

--- linked-list.js
+++ linked-list.js
@@ -1,16 +1,41 @@
 var LinkedList = (function() {
   var head = null;
 
-  var push = function(val) {
-    var newNode = function(val)
-      return (
-      data: val.
+  var newNode = function(val) {
+    return {
+      data: val,
       next: null
+    };
   };
 
+  var toString = function() {
+    var output = [];
+    var curr = head;
+    while (curr !== null) {
+      output.push(curr.data);
+      curr = curr.next;
+    }
+    
+    return output.join(" -> ");
   };
+
+   var push = function(val) {
+     var node = newNode(val);
+     if (head === null) {
+       head = node;
+       return;
+    }
+  };
+
   return {
-    push: push
+    push: push,
+    toString: toString
   };
-  
+
 }());
+
+var ll = LinkedList;
+
+ll.push("A");
+
+console.log(ll.toString()); 
